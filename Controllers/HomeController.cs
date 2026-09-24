89426fcd5bf5088323b0dfa00c592c34eb51520e
aymen <aymencvjaaa07@gmail.com>
2026-09-24 10:43:42 -1100

    Use main store page for game catalog

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayGames.Data;
+using PlayGames.Models;
 
 namespace PlayGames.Controllers;
 
-[Authorize]
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly StoreDbContext _db;
 
-    public IActionResult Privacy() => View();
+    public HomeController(StoreDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var model = new GameStoreViewModel
+        {
+            Games = await _db.Games
+                .Where(x => x.IsActive)
+                .OrderBy(x => x.Name)
+                .ToListAsync()
+        };
+
+        if (User.Identity?.IsAuthenticated == true)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!string.IsNullOrWhiteSpace(userId))
+            {
+                model.OwnedGameIds = (
+                    await _db.LibraryItems
+                        .Where(x => x.UserId == userId)
+                        .Select(x => x.GameId)
+                        .ToListAsync()
+                ).ToHashSet();
+            }
+        }
+
+        return View("~/Views/Games/Index.cshtml", model);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 }
